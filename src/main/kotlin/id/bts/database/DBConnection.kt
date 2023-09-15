fbdb3051a36222aa98b3052f36725e079ddc5d36
onedhonyreff* <onedhonyreff@gmail.com>
2023-09-15 11:18:27 +0700

    Adjust with online database

--- src/main/kotlin/id/bts/database/DBConnection.kt
+++ src/main/kotlin/id/bts/database/DBConnection.kt
@@ -6,21 +6,21 @@ import org.ktorm.support.postgresql.PostgreSqlDialect
 object DBConnection {
   val database = try {
     /* Local Server */
-    Database.connect(
-      url = "jdbc:postgresql:db_leave_app",
-      driver = "org.postgresql.Driver",
-      user = "postgres",
-      password = "dhony123",
-      dialect = PostgreSqlDialect()
-    )
+//    Database.connect(
+//      url = "jdbc:postgresql:db_leave_app",
+//      driver = "org.postgresql.Driver",
+//      user = "postgres",
+//      password = "dhony123",
+//      dialect = PostgreSqlDialect()
+//    )
 
     /* Online Server */
-//    Database.connect(
-//      url = "jdbc:postgresql://bubble.db.elephantsql.com:5432/pvhfrxwt",
-//      driver = "org.postgresql.Driver",
-//      user = "pvhfrxwt",
-//      password = "ky1wD1SqtG9IZLZpc6PHC2NvXyY-R30L"
-//    )
+    Database.connect(
+      url = "jdbc:postgresql://bubble.db.elephantsql.com:5432/pvhfrxwt",
+      driver = "org.postgresql.Driver",
+      user = "pvhfrxwt",
+      password = "ky1wD1SqtG9IZLZpc6PHC2NvXyY-R30L"
+    )
   } catch (e: Exception) {
     e.printStackTrace()
     null
